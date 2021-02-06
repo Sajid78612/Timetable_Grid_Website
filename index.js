1dd59ca976089a45f4d67b2163f41f84bb77f570
Sajid <sajidalam786@gmail.com>
2021-02-06 02:24:35 +0000

    So rafat doesnt vomit

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ var columnHeaders = document.getElementById('column-headers');
 var rowHeaders = document.getElementById('row-headers');
 
 function test() {
-document.write("<center><table border='1px'>");
-for (var a = 1; a < 13; a++) {
-  document.write("<tr style='height:40px'>");
-  for (var b = 1; b < 13; b++) {
-    document.write("<td style='width:40px'><center><font size='4'>" + a * b + "</center></font></td>");
-  }
-  document.write("</tr>");
-}
-document.write("</table></center>");
+	document.write("<center><table border='1px'>");
+	for (var a = 1; a < 13; a++) {
+		document.write("<tr style='height:40px'>");
+		for (var b = 1; b < 13; b++) {
+			document.write("<td style='width:40px'><center><font size='4'>" + a * b + "</center></font></td>");
+		}
+		document.write("</tr>");
+	}
+	document.write("</table></center>");
 }
 
 function createBoxes(min, max) {
@@ -19,7 +19,7 @@ function createBoxes(min, max) {
 	createRowHeaders(min, max);
 
 	for (let i = min; i <= max; i++) {
-		
+
 		for (let j = min; j <= max; j++) {
 			var box = document.createElement('INPUT');
 			//box.setAttribute('type', 'text');
